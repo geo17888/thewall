d1ed3c0161e667aca38007489188d1a773a60bc2
unknown <georgeyan17@gmail.com>
2020-05-30 23:43:36 +0100

    l

--- java.js
+++ java.js
@@ -12,7 +12,7 @@ function inputFunction(name){
    fetch("https://2ubiyjczwh.execute-api.eu-west-2.amazonaws.com/wall",{
        method: "POST" ,
        body: JSON.stringify(message:name.value})
-}).then(function (response)){
+}).then(function (response){
     if (response.status==200){
         console.log(response)
     }else{console.log("Oh No!")}
