c3c252a9a813be73cd224c8a3441471ab97d3db9
unknown <georgeyan17@gmail.com>
2020-05-30 23:05:52 +0100

    adf

---
+++ java.js
@@ -0,0 +1,23 @@
+fetch("https://2ubiyjczwh.execute-api.eu-west-2.amazonaws.com/wall").then(response => response.json()).then(response)
+function walldisplay(n){
+    let list1=document.getElementById("list");
+    n.foreach(items=>{
+        let submititem=document.createElement("div");
+        newitem.classlist.add("mystyle");
+        list1.appendChild(newitem);
+    })
+}
+
+function inputFunction(name){
+   fetch("https://2ubiyjczwh.execute-api.eu-west-2.amazonaws.com/wall",{
+       method: "POST" ,
+       body: JSON.stringify(message:name.value})
+}).then(function (response)){
+    if (response.status==200){
+        console.log(response)
+    }else{console.log("Oh No!")}
+    })
+    return false;
+}
+
+   
