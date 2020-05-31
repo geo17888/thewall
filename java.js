24b07b506e58bc151c86e9d688dee9e237d3f36f
unknown <georgeyan17@gmail.com>
2020-05-31 17:02:46 +0100

    asd

--- java.js
+++ java.js
@@ -10,10 +10,10 @@ function walldisplay(n){
 
 function inputFunction(name){
    fetch("https://2ubiyjczwh.execute-api.eu-west-2.amazonaws.com/wall",{
-       method: "POST" ,
+       method:"POST" ,
        body: JSON.stringify(message:name.value})
-}).then(function (response){
-    if (response.status==200){
+}).then(function (response) {
+    if (response.status == 200){
         console.log(response)
     }else{console.log("Oh No!")}
     })
