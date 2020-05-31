8d7b7aca8a3e9347aec10acd158d995d88acbfff
unknown <georgeyan17@gmail.com>
2020-05-31 17:10:28 +0100

    asd

--- java.js
+++ java.js
@@ -1,16 +1,17 @@
 fetch("https://2ubiyjczwh.execute-api.eu-west-2.amazonaws.com/wall").then(response => response.json()).then(response => listOfItems(response))
 function walldisplay(n){
     let list1 = document.getElementById("list");
-    n.forEach( items => {
+    n.forEach(items => {
         let submititem=document.createElement("div");
-        newitem.classlist.add("mystyle");
-        list1.appendChild(newitem);
+        submititem.innerText= items;
+        submititem.classList.add("mystyle");
+        list1.appendChild(submititem);
     })
 }
 
 function inputFunction(name){
    fetch("https://2ubiyjczwh.execute-api.eu-west-2.amazonaws.com/wall",{
-       method:"POST" ,
+       method:'POST',
        body: JSON.stringify(message:name.value})
 }).then(function (response) {
     if (response.status == 200){
