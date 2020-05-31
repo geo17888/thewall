b37f45c41a61afa1aac7bebcb6a85656ebb58d4d
unknown <georgeyan17@gmail.com>
2020-05-31 21:47:59 +0100

    wedf#

--- java.js
+++ java.js
@@ -1,5 +1,4 @@
-fetch('https://2ubiyjczwh.execute-api.eu-west-2.amazonaws.com/wall').then(response => response.json())
-.then(response => wallDisplay(response))
+fetch('https://2ubiyjczwh.execute-api.eu-west-2.amazonaws.com/wall').then(response => response.json()).then(response => wallDisplay(response))
 function wallDisplay(n)
 {
     let list1 = document.getElementById("list");
@@ -10,18 +9,17 @@ function wallDisplay(n)
         list1.appendChild(submitItem);
     });
 }
-}
+
 
 function inputFunction(name){
    fetch("https://2ubiyjczwh.execute-api.eu-west-2.amazonaws.com/wall",{
        method:'POST',
-       body: JSON.stringify(message:name.value})
+       body: JSON.stringify({message:name.value})
 }).then(function (response) {
     if (response.status == 200){
         console.log(response)
     }else{console.log("Oh No!")}
     })
-    return false;
 }
 
    
